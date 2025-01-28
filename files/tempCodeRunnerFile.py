f124f9bd05c8b58737f83b5f557eed1c705278f2
simmi5298 <104838695+simmi5298@users.noreply.github.com>
2025-01-28 22:53:47 +0530

    output files

---
+++ files/tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+with open('ICICI-Input-Case2.csv', mode='r') as file:
+    csv_reader = csv.reader(file)
+    rows = list(csv_reader)
+    
+with open('HDFC-Input-Case1.csv', mode='r') as file:
+    csv_reader = csv.reader(file)
+    rows = list(csv_reader)
+
+with open('IDFC-Input-Case4.csv', mode='r') as file:
+    csv_reader = csv.reader(file)
+    rows = list(csv_reader)
+
+with open('Axis-Input-Case3.csv', mode='r') as file:
+    csv_reader = csv.reader(file)
+    rows = list(csv_reader)
